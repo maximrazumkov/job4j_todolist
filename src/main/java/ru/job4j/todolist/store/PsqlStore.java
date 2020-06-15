d68abf66da3ca38376609bb46512b638a661e118
Maxim Razumkov <maxim.razumkov@gmail.com>
2020-06-15 16:59:10 +0300

    2. Лямбды и шаблон wrapper.[49295#279384]

--- src/main/java/ru/job4j/todolist/store/PsqlStore.java
+++ src/main/java/ru/job4j/todolist/store/PsqlStore.java
@@ -2,12 +2,14 @@ package ru.job4j.todolist.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.todolist.model.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class PsqlStore implements Store {
 
@@ -28,31 +30,33 @@ public class PsqlStore implements Store {
 
     @Override
     public List<Item> findAllTodo() {
-        List<Item> result = new ArrayList<>();
-        try {
-            Session session = this.sf.openSession();
-            session.beginTransaction();
-            result = session.createQuery("from ru.job4j.todolist.model.Item").list();
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.todolist.model.Item").list()
+        );
     }
 
     @Override
     public Integer createTodo(Item item) {
-        Integer result = null;
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            session.save(item);
-            session.getTransaction().commit();
-            result = item.getId();
+        return this.tx(
+                session -> {
+                    session.save(item);
+                    return item.getId();
+                }
+        );
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
             session.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return result;
     }
 }
